d45e52f51e0e5e1170c151ad9542e8759afcd48f
isataguchi <122779306+isataguchi@users.noreply.github.com>
2023-02-08 02:47:58 +0900

    Added current time zone

--- src/app.js
+++ src/app.js
@@ -10,23 +10,13 @@ function updateTime() {
       "hh:mm:ss[<small>]A[</small>]"
     );
   }
-
-  let newYorkElement = document.querySelector("#new-york");
-  let newYorkDateElement = newYorkElement.querySelector(".date");
-  let newYorkTimeELement = newYorkElement.querySelector(".time");
-  let newYorkTime = moment().tz("America/New_York");
-
-  newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
-  newYorkTimeELement.innerHTML = newYorkTime.format(
-    "hh:mm:ss[<small>]A[</small>]"
-  );
 }
-
-updateTime();
-setInterval(updateTime, 1000);
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
@@ -41,10 +31,11 @@ function updateCity(event) {
           )}<small>${cityTime.format("A")}</small></div>
           
         </div>
-  
-  
   `;
 }
+
+updateTime();
+setInterval(updateTime, 1000);
 
 let citiesSelectElement = document.querySelector("#city");
 citiesSelectElement.addEventListener("change", updateCity);
